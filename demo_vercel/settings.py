747afc66afaeb92bf95949baff730fd5b3fd6a66
codefyn <manis.shr@gmail.com>
2023-01-01 18:52:02 +0530

    ch3

--- demo_vercel/settings.py
+++ demo_vercel/settings.py
@@ -78,12 +78,7 @@ WSGI_APPLICATION = 'demo_vercel.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 
 # Password validation
