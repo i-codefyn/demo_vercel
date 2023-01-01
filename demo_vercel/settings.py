7c6376e35de8f7fd10039754ca57177e779451bb
codefyn <manis.shr@gmail.com>
2023-01-01 18:46:03 +0530

    ch2

--- demo_vercel/settings.py
+++ demo_vercel/settings.py
@@ -25,10 +25,10 @@ SECRET_KEY = 'django-insecure-0v14aih28%@)mego2=5=dy7#5510m4o@3+445jj9fvyrf59cku
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh','127.0.0.1']
 
 STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
 
 
 
